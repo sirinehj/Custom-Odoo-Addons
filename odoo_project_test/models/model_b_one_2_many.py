12e0196f070b0f8db0b0c747d2bea112e0638a30
abdelhamid2506 <mohamedabdelhamidfarhat@gmail.com>
2025-07-24 15:25:53 +0100

    chap-17-more buttons

--- odoo_project_test/models/model_b_one_2_many.py
+++ odoo_project_test/models/model_b_one_2_many.py
@@ -34,3 +34,46 @@ class Model_b_one_2_many(models.Model):
     #and model_b_one_2_many for manuel oredering
     _order="sequence desc"
     sequence=fields.Integer(default=1)
+    
+    #we will create button that will lead to all the specific vars of the relationshiin the other model
+    #in a curent page it's a nav button/redirction => we are taling about stat buttons
+    #first in the model we need the relationship var here
+    #we need 2 funcd one to open the view of the other linked model
+    #and the second func will show us how many records that linked model has
+    #so let's start with the first func
+    def action_open_model_a_ids(self):
+        #we will need self here just for it id so we don't need to loop
+        #the id will be set in the context and domain
+        #we will put the view config
+        return{
+            #dont forget to import _ from odoo in the top
+            "name":_("Model A one 2 many"),
+            "type":"ir.actions.act_window",
+            "view_mode":"list,form",
+            #the name of the other model of the relationship in this case is
+            #"model.a.one2many"
+            "res_model":"model.a.one2many",
+            #here will set the target page in the same page not new one
+            "target":"current",
+            #now the domain
+            #that will have the inverse var of the relationship in this model relation var:
+            #"model_b_one2many_id"
+            #it will be: "domain":[("model_b_one2many_id","=",self.id)]
+            "domain":[("model_b_one2many_id","=",self.id)],
+            #context will save the id in the new view action after the redirection by
+            #default_the inverse var of the relationship in this model relation var:
+            #"context":{"default_model_b_one2many_id":self.id}
+            "context":{"default_model_b_one2many_id":self.id}
+        }
+    #now the second func that will show us the number of records by it need a new var that will use 
+    #this func in compute param
+    model_b_one2many_count=fields.Integer(compute="_count_model_b_one2many_conut")
+    #the func
+    #this func need @api.depends() so import api from odoo in the top:
+    #in my case => @api.depends("model_a_ids")
+    @api.depends("model_a_ids")
+    def _count_model_b_one2many_conut(self):
+        for i in self:
+            #it will have the length of the relationship var  "model_a_ids"
+            i.model_b_one2many_count=len(i. model_a_ids)
+    #now to the view of this model in the form
--- projet_test/models/model_b_one_2_many.py
+++ projet_test/models/model_b_one_2_many.py
@@ -1,5 +1,5 @@
 #model_b_one_2_many linked with model_a_one_2_many by a one 2 many relation 
-from odoo import fields,models,api
+from odoo import fields,models,api,_
 
 class Model_b_one_2_many(models.Model):
     _name="model.b.one2many"
@@ -26,3 +26,20 @@ class Model_b_one_2_many(models.Model):
     #and model_b_one_2_many for manuel oredering
     _order="sequence desc"
     sequence=fields.Integer(default=1)
+
+    def action_open_model_a_ids(self):
+        return{
+            "name":_("Model A one 2 many"),
+            "type":"ir.actions.act_window",
+            "view_mode":"list,form",
+            "res_model":"model.a.one2many",
+            "target":"current",
+            "domain":[("model_b_one2many_id","=",self.id)],
+            "context":{"default_model_b_one2many_id":self.id}
+        }
+    model_b_one2many_count=fields.Integer(compute="_count_model_b_one2many_conut")
+    @api.depends("model_a_ids")
+    def _count_model_b_one2many_conut(self):
+        for i in self:
+            i.model_b_one2many_count=len(i. model_a_ids)
+    
