71bbd55fd55ad82124425e285c3e0af5f907df9f
Sirine hjaij <hjaijsirine@gmail.com>
2025-07-25 17:59:09 +0100

    added the short-cut buttons

--- Library/models/readingClub.py
+++ Library/models/readingClub.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class ReadingClub(models.Model):
     _name = "library.reading_club"
@@ -18,9 +18,29 @@ class ReadingClub(models.Model):
         string="Books Discussed",
         compute="_compute_books_discussed"
     )
+
+    meeting_count = fields.Integer(compute="_compute_meeting_count")
+
+
+    @api.depends('meeting_ids')
+    def _compute_meeting_count(self):
+        for rec in self:
+            rec.meeting_count = len(rec.meeting_ids)
     
     @api.depends('meeting_ids.club_id') 
     def _compute_books_discussed(self):
         for club in self:
             club.book_ids = club.meeting_ids.mapped('book_id')
+
+
+    def action_open_meeting_ids(self):
+        return {
+            "name": _("Related meetings"),
+            "type": "ir.actions.act_window",
+            "view_mode": "list,form",
+            "res_model": "library.meeting",
+            "target": "current",
+            "domain": [("club_id", "=", self.id)],
+            "context": {"default_club_id": self.id},
+        }
 
