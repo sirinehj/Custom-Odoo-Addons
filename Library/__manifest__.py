b4e79b02caa66c1735f114ab46097c8970726c70
Sirine hjaij <hjaijsirine@gmail.com>
2025-07-27 21:39:05 +0100

    Start of OWL Training

--- Library/__manifest__.py
+++ Library/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name" : "Library",
     "summary" : "Test Module",
-    "Version" : "18.0.0.0.0",
+    "Version" : "18.0.1.0.0",
     "license": "LGPL-3",
     "data" : [
         "security/res_groups.xml",
